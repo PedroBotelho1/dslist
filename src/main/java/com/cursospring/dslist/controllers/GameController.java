49b927cd2fdf22dfe13836f09132ade88bb47978
Pedro Botelho <botelhop573@gmail.com>
2025-01-22 21:34:14 -0300

    Game repository, service, controller

---
+++ src/main/java/com/cursospring/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.cursospring.dslist.controllers;
+
+import com.cursospring.dslist.DTO.GameMinDTO;
+import com.cursospring.dslist.entities.Game;
+import com.cursospring.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
